1463e30ebfa5dd29888deb997d1840d8738679be
Adriano Frota <adriano.frota.silva@avanade.com>
2019-05-23 23:39:54 -0300

    Blog
    React Tutorial - parte 1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import Button from 'react-bootstrap/Button';
+import {InputText} from 'primereact/inputtext';
+
+
+function App() {
+  return (
+    <div>
+      <Button primary> react bootstrap </Button>
+      <InputText value="InputText do Primefaces"/>
+    </div>
+  );
+}
+
+export default App;
