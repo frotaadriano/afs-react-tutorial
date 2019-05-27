c298c881245180bdf55e2b1d9f7ea11c5b4ede9e
Adriano Frota <adriano.frota.silva@avanade.com>
2019-05-26 21:25:09 -0300

    React Tutorial - Parte 2react, Routes, Tutorial

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
-import React from 'react'; 
-import Button from 'react-bootstrap/Button';
-import {InputText} from 'primereact/inputtext';
+import React from 'react';
 
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+import { InputText } from 'primereact/inputtext';
+
+
+import Sobre from './Pages/Sobre';
+import OutraPagina from './Pages/OutraPagina';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div>
-      <Button primary> react bootstrap </Button>
-      <InputText value="InputText do Primefaces"/>
-    </div>
+    <Router> 
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/sobre/">Sobre</Link>
+            </li>
+            <li>
+              <Link to="/outrapagina/">Outra Página</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <Route path="/" exact component={Home} />
+      <Route path="/sobre/" component={Sobre} />
+      <Route path="/outrapagina/" component={OutraPagina} />
+
+
+      <div>
+        <Button primary> react bootstrap </Button>
+        <InputText value="InputText do Primefaces" />
+      </div>
+    </Router>
+
   );
 }
 
---
+++ src/Pages/OutraPagina.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => (
+    <div>
+       Outra página 
+    </div>
+) 
